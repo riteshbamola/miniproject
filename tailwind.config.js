e4db157608def4c6f2e8347e80c333ece8f24015
RiteshBamola <riteshbamola121@gmail.com>
2023-12-31 21:53:34 +0530

    projectcreated

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:
+      {
+        'black-dark': '#00000050',
+        'dull-white': '#FFFFFFB3',
+        'white-light': '#FFFFFF30',
+        'white-medium': '#FFFFFF40',
+        'neon-white':'#2FB8FF',
+      }
+    },
+  },
+  plugins: [],
+}
+
